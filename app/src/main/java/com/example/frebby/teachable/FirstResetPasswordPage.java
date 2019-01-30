ecef28da7c819875b1af2a07a713cc3ad384e88e
Frebby98 <fredudoh33@gmail.com>
2019-01-30 12:36:46 +0100

    N.M.F.N

---
+++ app/src/main/java/com/example/frebby/teachable/FirstResetPasswordPage.java
@@ -0,0 +1,13 @@
+package com.example.frebby.teachable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstResetPasswordPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_reset_password_page);
+    }
+}
